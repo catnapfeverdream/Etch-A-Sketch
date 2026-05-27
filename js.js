4bfdb60727cf00c250b0ad499f9a6ea08d95a06b
meriemccown <255442697+catnapfeverdream@users.noreply.github.com>
2026-05-27 16:49:51 -0400

    fix div grid not appearing
    fix typo identifying divs as class in js and by id in css. grid visible.

--- js.js
+++ js.js
@@ -1,8 +1,10 @@
-// create square divs //
+   // create square divs //
 const totalDivs = 16; //change this number to scale the grid//
 const divContainer = document.getElementById("grid-container");
 
 
 for (let i=0; i < totalDivs; i++) {
-    divContainer.innerHTML += '<div class="grid-item"></div>';
-}
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('grid-item');
+    divContainer.appendChild(newDiv);
+};
