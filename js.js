8d55b2cb50e7849a17572bc27ecaa495c0a1d0fe
meriemccown <255442697+catnapfeverdream@users.noreply.github.com>
2026-05-29 18:28:43 -0400

    Add hover effect feature
    working grid with yellow mouse-over and no mouse-out

--- js.js
+++ js.js
@@ -5,6 +5,17 @@ const divContainer = document.getElementById("grid-container");
 
 for (let i=0; i < totalDivs; i++) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('grid-item');
+    newDiv.classList.add('grid-items');
     divContainer.appendChild(newDiv);
+    
+    // hover effect so grid divs change color when the mouse passes over them //
+
+    newDiv.addEventListener('mouseover', () => {
+        newDiv.style.backgroundColor = 'yellow';
+    });
+
+    newDiv.addEventListener('mouseout', () => {
+        newDiv.style.backgroundColor = '';
+    });
 };
+
