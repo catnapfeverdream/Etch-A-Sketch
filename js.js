d35721d4cd873a3cbf39b7d7a3e842f8e8f73036
meriemccown <255442697+catnapfeverdream@users.noreply.github.com>
2026-06-01 17:44:46 -0400

    Fix reset button prompt and execution
    Resolves issue of broken reset button - missing number change functionality.

--- js.js
+++ js.js
@@ -19,24 +19,35 @@ for (let i=0; i < totalDivs; i++) {
 
 const resetBtn = document.getElementById("reset-btn");
 
-resetBtn.addEventListener('click', () => {
+resetBtn.addEventListener('click', resetPrompt) 
     //send pop up prompting user to input number//
     function resetPrompt() {
         let text;
         let number = prompt('how many squares per side, my liege?', '25');
+         //set user input to max of 100 //
         if (number == null || number =='' || number > 100 === true) {
             text = 'err';
         } else {
-          let totalDivs = number;  
+         //multiply number by itself to make total div number and replace value of totalDivs//
+          number *= number;
+          //remove existing grid//
+          let oldGrid = document.querySelectorAll('.grid-items');
+          oldGrid.forEach(item => item.remove());
+
+          //generate new grid with new number of divs//
+          for (let i=0; i < totalDivs; i++) {
+            let totalDivs = number;
+            const newDiv = document.createElement('div');
+            const divContainer = document.getElementById("grid-container");
+            newDiv.classList.add('grid-items');
+            divContainer.appendChild(newDiv);
+            newDiv.addEventListener('mouseover', () => {
+            newDiv.style.backgroundColor = 'yellow';
+    });
+          }
+    
+        // hover effect so grid divs change color when the mouse passes over them //
         }
     }
 
-    //set user input to max of 100 //
-
-
-    //multiply number by itself to make total div number and replace value of totalDivs//
-
-    //remove existing grid//
-
-    //generate new grid with new number of divs//
-})
+   
