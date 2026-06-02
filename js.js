00bbef4f55a63a241db443a63077366667e355b2
meriemccown <255442697+catnapfeverdream@users.noreply.github.com>
2026-06-02 17:06:19 -0400

    Added features: opacity increase and random color
    replaces basic black fill in grid with randomized color with increasing opacity on every pass over.

--- js.js
+++ js.js
@@ -6,8 +6,8 @@ function createGrid(numDivs) {
 
     let squaresPerSide = Math.sqrt(numDivs);
     let boxSizePercentage = 100 / squaresPerSide;
-    let itemWidth = 'calc((90vw - 6px) / ${squaresPerSide})';
-    let itemHeight = 'calc((90vh - 6px) / ${squaresPerSide})';
+    let itemWidth = `calc((90vmin - 6px) / ${squaresPerSide})`;
+    let itemHeight = `calc((90vmin - 6px) / ${squaresPerSide})`;
 
     for (let i=0; i < numDivs; i++) {
         const newDiv = document.createElement('div');
@@ -17,14 +17,24 @@ function createGrid(numDivs) {
 
         divContainer.appendChild(newDiv);
 
-        newDiv.style.width = '${boxSizePercentage}%';
-        newDiv.style.height = '${boxSizePercentage}%';
+        newDiv.style.width = `${boxSizePercentage}%`;
+        newDiv.style.height = `${boxSizePercentage}%`;
         
         // hover effect so grid divs change color when the mouse passes over them //
 
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor = 'yellow';
-        });
+        newDiv.forEach((item) => {
+            // Basic Color: newDiv.style.backgroundColor = '#000000';//
+            //progressive darkening effect where each interaction darkens the square by 10%. achieve full color by 10 overs.//
+            item.addEventListener('mouseover', () => {
+                let currentOpacity = parseFloat(item.style.opacity) || 0;
+                
+                if (currentOpacity <1) {
+                    item.style.opacity = (currentOpacity + 0.1).toFixed(1);
+            }
+            })
+            
+            });
+            //randomize the squares' RGB values//
     };
 }
 
